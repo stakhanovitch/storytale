0b49c964d8c1c5009370557480d54b389c9828ac
STKHVTCH <valentin.reverdy@gmail.com>
2018-08-31 14:58:53 +0200

    MAJ HP

--- public/admin.py
+++ public/admin.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from public.models import Workshop
-from public.models import UserWorkshopRegistration
+from public.models import Workshop, Location, UserWorkshopRegistration
+
 admin.site.register(Workshop)
+admin.site.register(Location)
 admin.site.register(UserWorkshopRegistration)
 # Register your models here.
--- public/models/__init__.py
+++ public/models/__init__.py
@@ -1,2 +1,3 @@
+from .location import *
 from .workshop import *
-from .userworkshopregistration import *  
+from .userworkshopregistration import *
